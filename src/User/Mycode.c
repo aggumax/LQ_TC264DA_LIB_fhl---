38f832d5d0187c2dc5a82ee693657b440dccedba
Aoole Gumax <aggumax@outlook.com>
2023-10-30 00:18:47 +0800

    23.10.30

--- src/User/Mycode.c
+++ src/User/Mycode.c
@@ -12,16 +12,16 @@ uint8  Start_Flag2=0;                    //启动标志
 #define DUOJI_Kd      0;                //舵机PID参数
 
 float PINHEN_KP = 0, PINHEN_KI = 0, PINHEN_KD = 0;//平衡环PID调节,动量轮，pitch角度环
-float D_SPEED_KP=0, D_SPEED_KI=0,D_SPEED_KD=0;    //动量轮电机速度环PID控制
+float D_SPEED_KP=0, D_SPEED_KI=0;    //动量轮电机速度环PID控制
 
 /*****数值定义*****/
 int PWM_D, PWM_S;                       //PWM_D是动量轮电机控制左右倾斜，S是动量轮电机速度环正反馈
 int encValue_D = 0;                     //储存动量轮的编码器数值
 float Pitch_LINGDIAN = 1.5, Pitch_error2 = 0.00;//规定pitch的角度零点 1.5
 float Pitch_ERROR = 0.00;               //Pitch偏差值
+short 
 
-
-unsigned char Flag_Stop2 = 0;
+unsigned char Flag_Stop2 = 0;           //停车标识
 
 
 void Balance_FHL(void)
@@ -30,9 +30,22 @@ void Balance_FHL(void)
     ENC_InitConfig(ENC6_InPut_P20_3, ENC6_Dir_P20_0);//读取编码器数据
     encValue_D = ENC_GetCounter(ENC6_InPut_P20_3);    //动量轮的数值
 
-    /*动量轮控制*/
+    /*////动量轮控制////*/
     PWM_D = Balance_X(Pitch,Pitch_ERROR,gyro[0]);
-    PWM_S = SPEED_Control(encValue_D);
+    PWM_S = SPEED_Control(-encValue_D);
+
+    /*动量轮限幅*/
+    if(PWM_D>8000) 
+        PWM_D=8000;
+    if(PWM_D<-8000) 
+        PWM_D=-8000;
+    /*////停车控制////*/
+    if((Pitch > 23) || (Pitch < -23)) //摔倒判断
+    Flag_Stop2 = 1;
+    if(Flag_Stop2 == 1)               //停车
+    {
+
+    }
 }
 
 
@@ -40,7 +53,7 @@ void Balance_FHL(void)
 平衡PID控制函数，角度环
 Angle:  Pitch的角度
 Angle_Zero:  Pitch的偏差值
-Gyro;  Gyro[]
+Gyro:  Gyro[]
  ******************/
 float Balance_X(float Angle,float Angle_Zero,float Gyro)
 {
@@ -58,7 +71,7 @@ float Balance_X(float Angle,float Angle_Zero,float Gyro)
 
 /*******************
 动量轮速度PI控制,速度正反馈环
-encValueD;  编码器的数值(速度环)
+encValueD:  编码器的数值(速度环)
  ******************/
 float SPEED_Control(int encValueD)
 {
@@ -73,6 +86,7 @@ float SPEED_Control(int encValueD)
     if(Encoder_Integral < -2000) Encoder_Integral = -2000;                    //积分限幅
     Velocity = Encoder * D_SPEED_KP + Encoder_Integral * D_SPEED_KI/100;      //获取最终数值
     if(Flag_Stop2==1) Encoder_Integral=0,Encoder=0,Velocity=0;                 //停止时参数清零
+    
     return Velocity;
 }
 
